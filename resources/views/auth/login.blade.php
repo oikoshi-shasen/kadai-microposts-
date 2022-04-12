87793dfda407326870117dc1e888230d9bd7a905
yudai.sasaki <yudai_sasaki@fandmnet.com>
2022-04-12 08:33:07 +0000

    user pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                     {!! Form::label('email', 'Email') !!}
                     {!! Form::email('email', null, ['class' => 'form-control']) !!}
                 </div>
-
+            
                 <div class="form-group">
                     {!! Form::label('password', 'Password') !!}
                     {!! Form::password('password', ['class' => 'form-control']) !!}
