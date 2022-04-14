c1e868a7c4ec2d9da893cd7bb40dbf1edfa975bf
yudai.sasaki <yudai_sasaki@fandmnet.com>
2022-04-14 01:58:08 +0000

    post

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,21 +1,38 @@
-@if (count($users) > 0)
-    <ul class="list-unstyled">
-        @foreach ($users as $user)
-            <li class="media">
-                {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
-                <img class="mr-2 rounded" src="{{ Gravatar::get($user->email, ['size' => 50]) }}" alt="">
-                <div class="media-body">
-                    <div>
-                        {{ $user->name }}
-                    </div>
-                    <div>
-                        {{-- ユーザ詳細ページへのリンク --}}
-                        <p>{!! link_to_route('users.show', 'View profile', ['user' => $user->id]) !!}</p>
-                    </div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">{{ $user->name }}</h3>
                 </div>
-            </li>
-        @endforeach
-    </ul>
-    {{-- ページネーションのリンク --}}
-    {{ $users->links() }}
-@endif
+                <div class="card-body">
+                    {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
+                    <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
+                </div>
+            </div>
+        </aside>
+        <div class="col-sm-8">
+            <ul class="nav nav-tabs nav-justified mb-3">
+                {{-- ユーザ詳細タブ --}}
+                <li class="nav-item">
+                    <a href="{{ route('users.show', ['user' => $user->id]) }}" class="nav-link {{ Request::routeIs('users.show') ? 'active' : '' }}">
+                        TimeLine
+                        <span class="badge badge-secondary">{{ $user->microposts_count }}</span>
+                    </a>
+                </li>
+                {{-- フォロー一覧タブ --}}
+                <li class="nav-item"><a href="#" class="nav-link">Followings</a></li>
+                {{-- フォロワー一覧タブ --}}
+                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+            </ul>
+            @if (Auth::id() == $user->id)
+                {{-- 投稿フォーム --}}
+                @include('microposts.form')
+            @endif
+            {{-- 投稿一覧 --}}
+            @include('microposts.microposts')
+        </div>
+    </div>
+@endsection
